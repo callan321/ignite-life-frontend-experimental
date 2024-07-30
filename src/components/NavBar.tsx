98a9e3b315981d11ee7959041e1edec7ea950a3e
callan321 <callan.mitchell11@gmail.com>
2024-07-31 09:51:56 +1000

    Updated NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import {Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react'
+import {Bars3Icon, BellIcon, XMarkIcon} from '@heroicons/react/24/outline'
 
 interface NavBarProps {
     items: Item[];
 }
 
 interface Item {
-    name : string,
+    name: string,
     link: string,
 }
 
@@ -18,11 +18,12 @@ export default function NavBar({items}: NavBarProps) {
                 <div className="relative flex h-16 justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* Mobile menu button */}
-                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                            <span className="absolute -inset-0.5" />
+                        <DisclosureButton
+                            className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="absolute -inset-0.5"/>
                             <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
-                            <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+                            <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden"/>
+                            <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block"/>
                         </DisclosureButton>
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -51,16 +52,17 @@ export default function NavBar({items}: NavBarProps) {
                             type="button"
                             className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
-                            <span className="absolute -inset-1.5" />
+                            <span className="absolute -inset-1.5"/>
                             <span className="sr-only">View notifications</span>
-                            <BellIcon aria-hidden="true" className="h-6 w-6" />
+                            <BellIcon aria-hidden="true" className="h-6 w-6"/>
                         </button>
 
                         {/* Profile dropdown */}
                         <Menu as="div" className="relative ml-3">
                             <div>
-                                <MenuButton className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                    <span className="absolute -inset-1.5" />
+                                <MenuButton
+                                    className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                    <span className="absolute -inset-1.5"/>
                                     <span className="sr-only">Open user menu</span>
                                     <img
                                         alt=""
@@ -74,17 +76,20 @@ export default function NavBar({items}: NavBarProps) {
                                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                             >
                                 <MenuItem>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
                                         Your Profile
                                     </a>
                                 </MenuItem>
                                 <MenuItem>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
                                         Settings
                                     </a>
                                 </MenuItem>
                                 <MenuItem>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
                                         Sign out
                                     </a>
                                 </MenuItem>
@@ -96,35 +101,17 @@ export default function NavBar({items}: NavBarProps) {
 
             <DisclosurePanel className="sm:hidden">
                 <div className="space-y-1 pb-4 pt-2">
-                    {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-                    >
-                        Dashboard
-                    </DisclosureButton>
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-                    >
-                        Team
-                    </DisclosureButton>
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-                    >
-                        Projects
-                    </DisclosureButton>
-                    <DisclosureButton
-                        as="a"
-                        href="#"
-                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-                    >
-                        Calendar
-                    </DisclosureButton>
+                    {items.map((item) => (
+                        <DisclosureButton
+                            as="a"
+                            key={item.name}
+                            href={item.link}
+                            className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                        >
+                            {item.name}
+                        </DisclosureButton>
+
+                    ))}
                 </div>
             </DisclosurePanel>
         </Disclosure>
