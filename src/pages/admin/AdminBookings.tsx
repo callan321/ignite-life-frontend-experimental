f7585943b25d6038e157fb99bce337048331bf50
callan321 <callan.mitchell11@gmail.com>
2024-08-01 22:08:36 +1000

    Added calender template

--- src/pages/admin/AdminBookings.tsx
+++ src/pages/admin/AdminBookings.tsx
@@ -1,7 +1,9 @@
+import AdminCalendar from "../../components/admin/AdminCalendar.tsx";
+
 export default function AdminBookings() {
     return (
-        <div className="flex justify-center p-12">
-            <h1>This page will have a calendar view of upcoming bookings</h1>
+        <div >
+            <AdminCalendar/>
         </div>
     )
 }
